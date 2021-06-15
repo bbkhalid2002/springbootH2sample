b383288bec88a2a2776bdab8a9c6ad4b947f82d3
bbkhalid2002 <75424759+bbkhalid2002@users.noreply.github.com>
2021-06-15 17:39:45 +0300

    Init

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	@GetMapping
+	public List<Map<String, Object>> index() {
+		return jdbcTemplate.queryForList("SELECT * FROM customers");
+
+	}
+}
